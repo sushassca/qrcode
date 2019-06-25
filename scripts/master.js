6a40a34ffa7a7c614a195bf42ddcca91ef860380
Alexandre Graça <49596963+sushassca@users.noreply.github.com>
2019-06-25 10:21:24 +0100

    scan

--- scripts/master.js
+++ scripts/master.js
@@ -4,23 +4,20 @@ $(document).ready(function() {
     $("#mobile").show();
     $("#web").hide();
 
-    let scanner = new Instascan.Scanner(
-             {
-                 video: document.getElementById('preview')
-             }
-         );
-         scanner.addListener('scan', function(content) {
-             alert('Escaneou o conteudo: ' + content);
-             window.open(content, "_blank");
-         });
-         Instascan.Camera.getCameras().then(cameras =>
-         {
-             if(cameras.length > 0){
-                 scanner.start(cameras[0]);
-             } else {
-                 console.error("Não existe câmera no dispositivo!");
-             }
-         });
+    let scanner = new Instascan.Scanner({
+      video: document.getElementById('preview')
+    });
+    scanner.addListener('scan', function(content) {
+      alert('Escaneou o conteudo: ' + content);
+      window.open(content, "_blank");
+    });
+    Instascan.Camera.getCameras().then(cameras => {
+      if (cameras.length > 0) {
+        scanner.start(cameras[0]);
+      } else {
+        console.error("Não existe câmera no dispositivo!");
+      }
+    });
 
 
   } else {
