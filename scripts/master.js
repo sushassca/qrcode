b749a85730767b6095ae1a87d9ba4c65bf91c489
Alexandre Graça <49596963+sushassca@users.noreply.github.com>
2019-06-25 09:56:36 +0100

    mobile or pc?

--- scripts/master.js
+++ scripts/master.js
@@ -1,21 +1,34 @@
 $(document).ready(function() {
-  function c(b) {
-    jQuery(function() {
-      jQuery("#output").qrcode({render:"table", text:b});
-    });
-  }
 
-  $("div.lis").bind("input", function() {
-    var b = $("#value").val(), a = $("#pass").val();
-    b = CryptoJS.AES.encrypt(b, a);
-    $("#encrypted").val(b);
-    a = CryptoJS.AES.decrypt(b, a);
-    $("#decrypted").val(a);
-    a = a.toString(CryptoJS.enc.Utf8);
-    $("#result").val(a);
-    $("#output table").remove();
-    c($("#encrypted").val());
-  });
-  
-  c("  ");
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    $("#mobile").show();
+    $("#web").hide();
+  } else {
+    $("#web").show();
+    $("#mobile").hide();
+
+    function c(b) {
+      jQuery(function() {
+        jQuery("#output").qrcode({
+          render: "table",
+          text: b
+        });
+      });
+    }
+
+    $("div.lis").bind("input", function() {
+      var b = $("#value").val(),
+        a = $("#pass").val();
+      b = CryptoJS.AES.encrypt(b, a);
+      $("#encrypted").val(b);
+      a = CryptoJS.AES.decrypt(b, a);
+      $("#decrypted").val(a);
+      a = a.toString(CryptoJS.enc.Utf8);
+      $("#result").val(a);
+      $("#output table").remove();
+      c($("#encrypted").val());
+    });
+
+    c("  ");
+  }
 });
