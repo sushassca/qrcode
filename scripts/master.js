ea740c79342630a62fae95f6944ba1aaddcb7aad
Alexandre Graça <49596963+sushassca@users.noreply.github.com>
2019-06-25 10:35:14 +0100

    back

--- scripts/master.js
+++ scripts/master.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
       var video = document.getElementById('preview');
       // Not adding `{ audio: true }` since we only want video now
       navigator.mediaDevices.getUserMedia({
-        video: true
+        video: { facingMode: { exact: "environment" } }
       }).then(function(stream) {
         //video.src = window.URL.createObjectURL(stream);
         video.srcObject = stream;
