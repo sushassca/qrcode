0eb5b8a9fe8d4f11e1ba413529ab887b10ba5376
Alexandre Graça <49596963+sushassca@users.noreply.github.com>
2019-06-25 10:57:01 +0100

    teste4

--- scripts/master.js
+++ scripts/master.js
@@ -4,86 +4,14 @@ $(document).ready(function() {
     $("#mobile").show();
     $("#web").hide();
 
-    var videoSelect = document.querySelector("select#videoSource");
-    var selectors = [videoSelect];
+    const constraints = {
+      video: true
+    };
 
-    function gotDevices(deviceInfos) {
-      // Handles being called several times to update labels. Preserve values.
-      var values = selectors.map(function(select) {
-        return select.value;
-      });
-      selectors.forEach(function(select) {
-        while (select.firstChild) {
-          select.removeChild(select.firstChild);
-        }
-      });
+    const video = document.querySelector('video');
 
-      for (var i = 0; i !== deviceInfos.length; ++i) {
-        var deviceInfo = deviceInfos[i];
-        var option = document.createElement("option");
-        option.value = deviceInfo.deviceId;
-
-        if (deviceInfo.kind === "videoinput") {
-          option.text = deviceInfo.label || "camera " + (videoSelect.length + 1);
-          videoSelect.appendChild(option);
-        } else {
-          console.log("Some other kind of source/device: ", deviceInfo);
-        }
-
-        selectors.forEach(function(select, selectorIndex) {
-          if (
-            Array.prototype.slice.call(select.childNodes).some(function(n) {
-              return n.value === values[selectorIndex];
-            })
-          ) {
-            select.value = values[selectorIndex];
-          }
-        });
-      }
-    }
-
-    navigator.mediaDevices
-      .enumerateDevices()
-      .then(gotDevices)
-      .catch(handleError);
-
-    function gotStream(stream) {
-      arToolkitSource.domElement.srcObject = stream; // make stream available to console
-      var  video =  document.getElementById('preview');
-      video.srcObject = stream;
-      // Refresh button list in case labels have become available
-      return navigator.mediaDevices.enumerateDevices();
-    }
-
-    function start() {
-      if (window.stream) {
-        window.stream.getTracks().forEach(function(track) {
-          track.stop();
-        });
-      }
-      var videoSource = videoSelect.value;
-      var constraints = {
-        video: {
-          deviceId: videoSource ? {
-            exact: videoSource
-          } : undefined
-        }
-      };
-      navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(gotStream)
-        .then(gotDevices)
-        .catch(handleError);
-    }
-
-    videoSelect.onchange = start;
-
-    function handleError(error) {
-      console.log("navigator.getUserMedia error: ", error);
-    }
-
-    start();
-
+    navigator.mediaDevices.getUserMedia(constraints).
+      then((stream) => {video.srcObject = stream});
 
     let scanner = new Instascan.Scanner({
       video: document.getElementById('preview')
