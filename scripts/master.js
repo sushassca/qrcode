48016d5bf87d22a7920aaca182e3dd16b468c0f5
Alexandre Graça <49596963+sushassca@users.noreply.github.com>
2019-06-25 10:50:51 +0100

    teste2

--- scripts/master.js
+++ scripts/master.js
@@ -4,34 +4,85 @@ $(document).ready(function() {
     $("#mobile").show();
     $("#web").hide();
 
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      var video = document.getElementById('preview');
+    var videoSelect = document.querySelector("select#videoSource");
+    var selectors = [videoSelect];
+
+    function gotDevices(deviceInfos) {
+      // Handles being called several times to update labels. Preserve values.
+      var values = selectors.map(function(select) {
+        return select.value;
+      });
+      selectors.forEach(function(select) {
+        while (select.firstChild) {
+          select.removeChild(select.firstChild);
+        }
+      });
+
+      for (var i = 0; i !== deviceInfos.length; ++i) {
+        var deviceInfo = deviceInfos[i];
+        var option = document.createElement("option");
+        option.value = deviceInfo.deviceId;
+
+        if (deviceInfo.kind === "videoinput") {
+          option.text = deviceInfo.label || "camera " + (videoSelect.length + 1);
+          videoSelect.appendChild(option);
+        } else {
+          console.log("Some other kind of source/device: ", deviceInfo);
+        }
+
+        selectors.forEach(function(select, selectorIndex) {
+          if (
+            Array.prototype.slice.call(select.childNodes).some(function(n) {
+              return n.value === values[selectorIndex];
+            })
+          ) {
+            select.value = values[selectorIndex];
+          }
+        });
+      }
+    }
+
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then(gotDevices)
+      .catch(handleError);
+
+    function gotStream(stream) {
+      arToolkitSource.domElement.srcObject = stream; // make stream available to console
+      // video.srcObject = stream;
+      // Refresh button list in case labels have become available
+      return navigator.mediaDevices.enumerateDevices();
+    }
+
+    function start() {
+      if (window.stream) {
+        window.stream.getTracks().forEach(function(track) {
+          track.stop();
+        });
+      }
+      var videoSource = videoSelect.value;
       var constraints = {
         video: {
-          width: {
-            min: 1280,
-            ideal: 1920,
-            max: 2560,
-          },
-          height: {
-            min: 720,
-            ideal: 1080,
-            max: 1440
-          },
-          facingMode: {
-            exact: 'environment'
-          }
+          deviceId: videoSource ? {
+            exact: videoSource
+          } : undefined
         }
       };
-      // Not adding `{ audio: true }` since we only want video now
-      navigator.mediaDevices.getUserMedia(constraints)
-        .then(function(mediaStream) {
-          video.srcObject = mediaStream;
-          video.onloadedmetadata = function(e) {
-            video.play();
-          };
-        })
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(gotStream)
+        .then(gotDevices)
+        .catch(handleError);
     }
+
+    videoSelect.onchange = start;
+
+    function handleError(error) {
+      console.log("navigator.getUserMedia error: ", error);
+    }
+
+    start();
+
 
     let scanner = new Instascan.Scanner({
       video: document.getElementById('preview')
