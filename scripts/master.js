1a6aff4da69611a6f5fc39d447e92852c42b93f4
Alexandre Graça <49596963+sushassca@users.noreply.github.com>
2019-06-25 10:04:15 +0100

    camera

--- scripts/master.js
+++ scripts/master.js
@@ -3,6 +3,22 @@ $(document).ready(function() {
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     $("#mobile").show();
     $("#web").hide();
+
+    let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+     scanner.addListener('scan', function (content) {
+       console.log(content);
+     });
+     Instascan.Camera.getCameras().then(function (cameras) {
+       if (cameras.length > 0) {
+         scanner.start(cameras[0]);
+       } else {
+         console.error('No cameras found.');
+       }
+     }).catch(function (e) {
+       console.error(e);
+     });
+
+
   } else {
     $("#web").show();
     $("#mobile").hide();
