543f1fd543ea07f8539ca940cba6215654ffe1fe
Alexandre Graça <49596963+sushassca@users.noreply.github.com>
2019-06-27 17:31:42 +0100

    asdasdsa

--- scripts/master.js
+++ scripts/master.js
@@ -25,11 +25,12 @@ $(document).ready(function() {
     function myRequest() {
       req.onreadystatechange = () => {
         if (req.readyState == XMLHttpRequest.DONE) {
+          alert(data.result.request_deviceID)
+          mydeviceidAPI = data.result.request_deviceID;
           let data = JSON.parse(req.responseText);
           if (data.result.request_status == 1) {
             if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-              alert(data.result.request_deviceID)
-              mydeviceidAPI = data.result.request_deviceID;
+
             } else {
               alert("DONE");
             };
