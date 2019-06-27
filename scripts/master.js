8b6a8c26c32c4e6276729329297c5bd4032af2bd
Alexandre Graça <49596963+sushassca@users.noreply.github.com>
2019-06-27 15:24:45 +0100

    asfsd

--- scripts/master.js
+++ scripts/master.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
     req.setRequestHeader("secret-key", "$2a$10$EVkvuBx5r5NbXv/NgGsVOuUdV1YmUwvo6gCwejsk5tvOn5JrSuh4y");
     req.send();
   }
-  var myRequest = setInterval(myRequest, 2000);
+  var myRequest = setInterval(myRequest, 10000);
 
 
   function update(number) {
